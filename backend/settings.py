f49bd22ca3868a1fe2b1bc9756b9fdc13078a828
Shunsuke MOROZUMI <morozumi@MOROZUMIs-M3-MacBook-Air.local>
2025-05-17 01:32:35 +0900

    create Repository

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
